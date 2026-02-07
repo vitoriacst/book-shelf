a51ec87a734851fc3f96ddb893ea8346e1489137
vitoriacst <vitoria.cst.holanda@gmail.com>
2026-02-07 17:26:19 -0300

    feat: :sparkles: creating footer component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [ HeaderComponent],
+  imports: [ HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
